ff38dbb510c70c2cf943038ba4126093a7ab4eb2
miastefanec <mstefanec1@etfos.hr>
2021-09-03 11:00:06 +0200

    initial commit

---
+++ FPV Drone Controller/Assets/Drones/DroneController2.0/Scripts/DroneMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using DroneController.Physics;
+
+public class DroneMovement : DroneMovementScript {
+
+    public override void Awake()
+    {
+        base.Awake(); //I would suggest you to put code below this line
+    }
+
+	public override void Start()
+	{
+		base.Start(); //I would suggest you to put code below this line
+	}
+
+	public override void FixedUpdate()
+    {
+		base.FixedUpdate(); //I would suggest you to put code below this line
+	}
+
+	public override void Update ()
+	{
+		base.Update(); //I would suggest you to put code below this line
+
+		SceneChangeOnClick();
+		FlipDroneOnClick();
+	}
+
+//#################################################################################################################################################################################
+//My scripts...
+//#################################################################################################################################################################################
+
+	private void SceneChangeOnClick()
+	{
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.JoystickButton0)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	private void FlipDroneOnClick()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton1)) FlipDrone();
+	}
+
+}
